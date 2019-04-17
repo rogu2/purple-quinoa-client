eee42a04eb0c880b444dffa6f00d954c64efc851
rogu2 <rguempel@gmail.com>
2019-04-17 14:31:37 -0400

    Enable Index Action
    - update set state in promise chain to collect correct data
    - update render fragment to display correct response data

--- src/recipes/components/Recipes.js
+++ src/recipes/components/Recipes.js
@@ -2,46 +2,57 @@ import React, { Component, Fragment } from 'react'
 // import axios from 'axios'
 // import apiUrl from '../../apiConfig'
 import { Link } from 'react-router-dom'
+// import Alert from 'react-bootstrap/Alert'
 import { getRecipes } from '../RecipeAjax'
-import Spinner from 'react-bootstrap/Spinner'
+// import Spinner from 'react-bootstrap/Spinner'
 
 class Recipes extends Component {
   constructor () {
     super()
 
     this.state = {
-      recipes: []
+      recipes: [],
+      recipe: null,
+      title: null,
+      notes: null,
+      ingredient: null
     }
   }
 
   componentDidMount () {
     console.log('recipes component mounted')
-    let recipeData
+    // let recipeData
     getRecipes(this.props.user)
-      .then(response => {
-        recipeData = response.data.recipes
-        this.setState({
-          recipes: recipeData
-        })
-      })
+      .then(response => { this.setState({ recipes: response.data.recipe }) })
       .catch(console.error)
   }
 
   render () {
     const { recipes } = this.state
 
-    if (recipes.length === 0) {
-      return <Spinner animation="border" />
-    }
+    // const { title, notes, ingredient } = this.recipes
+    // console.log(title, notes, ingredient)
+
+    // if (this.state.recipes.length === 0) {
+    //   return <Spinner animation="border" />
+    // }
+    //
+    // if (recipes.length === null) {
+    //   return <p>{recipes.length === 0 ? <Alert variant="warning">{'No recipes to display'}</Alert> : ''}</p>
+    // }
     console.log('recipes component render')
     return (
       <Fragment>
         <h4> Recipes: </h4>
-        <ul>{recipes.map(recipe => (
-          <li key={recipe.title}>
-            <Link to={'/recipes/' + recipe.id}>{recipe.title}</Link>
-          </li>))}
-        </ul>
+        {recipes.map(recipes => (
+          <ul key={recipes._id}>
+            <li>
+              <Link to={'/recipes/' + recipes._id}>{recipes.title}</Link>
+            </li>
+            <li>ingredient: {recipes.ingredient}</li>
+            <li>notes: {recipes.notes}</li>
+          </ul>
+        ))}
       </Fragment>
     )
   }
